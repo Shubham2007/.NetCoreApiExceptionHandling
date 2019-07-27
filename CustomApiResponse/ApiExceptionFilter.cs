aff1bb30c8ab95f618ae68feaaf192ec189c6588
shubham.chauhan@izenda.com <shubham.chauhan@izenda.com>
2019-07-27 19:23:50 +0530

    Added Exception filter to handled exception in Web API

---
+++ CustomApiResponse/ApiExceptionFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+//using System.Web.Mvc;
+
+namespace CustomApiResponse
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            //Log exception
+            HandleExceptionAsync(filterContext);
+            filterContext.ExceptionHandled = true;
+        }
+
+        private static void HandleExceptionAsync(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            if (exception is UnauthorizedAccessException)
+                SetExceptionResult(context, exception, HttpStatusCode.Unauthorized);
+            else if (exception is Exception)
+                SetExceptionResult(context, exception, HttpStatusCode.BadRequest);
+            else
+                SetExceptionResult(context, exception, HttpStatusCode.InternalServerError);
+        }
+
+        private static void SetExceptionResult(
+        ExceptionContext context,
+        Exception exception,
+        HttpStatusCode code)
+        {
+            context.Result = new JsonResult(new ApiResonse
+            {
+                StatusCode = (int)code,
+                ErrorMessage = exception.Message,
+                Response = null
+            });
+        }
+    }
+}
---
+++ CustomApiResponse/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomApiResponse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new Exception();
+            return new JsonResult(new ApiResonse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                ErrorMessage = null,
+                Response = new string[] {"Magnum", "Tuborg"}
+            });
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
